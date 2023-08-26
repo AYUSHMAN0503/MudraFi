b38c7dedbcaa44cc5313ea858820024d20037c47
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-26 19:32:39 +0530

    Feat: Added Hero section in MudraFi

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { NavbarDefault } from './Components/Navbar'
+import Home from './Components/Hero'
 const App = () => {
   return (
     <div>
 <NavbarDefault/>
+<Home/>
     </div>
   )
 }
