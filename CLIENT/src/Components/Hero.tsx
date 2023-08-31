3adf5765bb2294b2162452511e0c86005521f9dd
PiyushXmishra <piyushbhp83@gmail.com>
2023-08-31 13:27:34 +0530

    Feat:Added Footer Section

--- CLIENT/src/Components/Hero.tsx
+++ CLIENT/src/Components/Hero.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import Typewriter from 'typewriter-effect';
 
 
 const Home: React.FC = () => {
     return (
         <div className="relative" id="home">
-            <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+            <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20 ">
                 <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
                 <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
             </div>
             <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
                 <div className="relative pt-36 ml-auto">
                     <div className="lg:w-2/3 text-center mx-auto">
-                        <h1 className="text-white font-bold text-4xl md:text-6xl xl:text-7xl">A Protocol for investing trading and<span className="text-white"> asset management.</span></h1>
+                        <h1 className="text-white font-bold text-4xl md:text-6xl xl:text-7xl">
+
+                            A Protocol for
+                            <Typewriter
+                                options={{
+                                    strings: ["Investing", "Trading", "Asset Management"],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            /></h1>
                         <p className="mt-8 text-white dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>
                         <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
                             <a
