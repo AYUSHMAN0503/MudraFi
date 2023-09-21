0002d525722255ab6d2c51d4be167e855a4d3171
YashXBansal <yashbansal414@gmail.com>
2023-09-21 19:28:56 +0530

    changes in swapinterface and web3 folder

--- WEB3/scripts/deploy.js
+++ WEB3/scripts/deploy.js
@@ -1,67 +1,43 @@
 // const hre = require("hardhat");
 
 async function main(){
-  // ERC20 - Boo token
-  const BooToken = await hre.ethers.getContractFactory("BooToken");
-  const booToken = await BooToken.deploy();
-  await booToken.deployed();
-  console.log(`BOO deployed to ${booToken.address}`);
-
-  // // bootoken
-  //   const [BooToken] = await ethers.getSigners();
+  // bootoken
+    const [BooToken] = await ethers.getSigners();
   
-  //   console.log("Deploying contracts with the account:", BooToken.address);
+    console.log("Deploying contracts with the account:", BooToken.address);
   
-  //   const booToken = await ethers.deployContract("BooToken");
+    const booToken = await ethers.deployContract("BooToken");
   
-  //   console.log("Boo Token address:", await booToken.getAddress());
+    console.log("Boo Token address:", await booToken.getAddress());
   
-  // // Lifetoken
-  //   const [LifeToken] = await ethers.getSigners();
+  // Lifetoken
+    const [LifeToken] = await ethers.getSigners();
   
-  //   console.log("Deploying contracts with the account:", LifeToken.address);
+    console.log("Deploying contracts with the account:", LifeToken.address);
   
-  //   const lifeToken = await ethers.deployContract("LifeToken");
+    const lifeToken = await ethers.deployContract("LifeToken");
   
-  //   console.log("Life Token address:", await lifeToken.getAddress());
-
-  // // SingleSwapToken
-
-  //   const [SingleSwapToken] = await ethers.getSigners();
-  
-  //   console.log("Deploying contracts with the account:", SingleSwapToken.address);
-  
-  //   const singleSwapToken = await ethers.deployContract("SingleSwapToken");
-  
-  //   console.log("SingleSwap address:", await singleSwapToken.getAddress());
-
-  // // SwapMultiHop
-  //   const [SwapMultiHop] = await ethers.getSigners();
-  
-  //   console.log("Deploying contracts with the account:", SwapMultiHop.address);
-  
-  //   const swapMultiHop = await ethers.deployContract("SwapMultiHop");
-  
-  //   console.log("MultiHopSwap address:", await swapMultiHop.getAddress());
-  
-
-  // ERC20 - Life token
-  const LifeToken = await hre.ethers.getContractFactory("BooToken");
-  const lifeToken = await LifeToken.deploy();
-  await lifeToken.deployed();
-  console.log(`Life deployed to ${lifeToken.address}`);
+    console.log("Life Token address:", await lifeToken.getAddress());
 
   // SingleSwapToken
-  const SingleSwapToken = await hre.ethers.getContractFactory("BooToken");
-  const singleSwapToken = await SingleSwapToken.deploy();
-  await singleSwapToken.deployed();
-  console.log(`Life deployed to ${singleSwapToken.address}`);
+
+    const [SingleSwapToken] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", SingleSwapToken.address);
+  
+    const singleSwapToken = await ethers.deployContract("SingleSwapToken");
+  
+    console.log("SingleSwap address:", await singleSwapToken.getAddress());
 
   // SwapMultiHop
-  const SwapMultiHop = await hre.ethers.getContractFactory("BooToken");
-  const swapMultiHop = await SwapMultiHop.deploy();
-  await swapMultiHop.deployed();
-  console.log(`Life deployed to ${swapMultiHop.address}`);
+    const [SwapMultiHop] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", SwapMultiHop.address);
+  
+    const swapMultiHop = await ethers.deployContract("SwapMultiHop");
+  
+    console.log("MultiHopSwap address:", await swapMultiHop.getAddress());
+
 };
 
 main()
