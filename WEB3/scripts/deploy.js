8bf28e03f72290da82f7317ca8ece166ede2c63e
YashXBansal <yashbansal414@gmail.com>
2023-09-29 20:14:25 +0530

    tried swapping

--- WEB3/scripts/deploy.js
+++ WEB3/scripts/deploy.js
@@ -1,23 +1,6 @@
 // const hre = require("hardhat");
 
 async function main(){
-  // bootoken
-    const [BooToken] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", BooToken.address);
-  
-    const booToken = await ethers.deployContract("BooToken");
-  
-    console.log("Boo Token address:", await booToken.getAddress());
-  
-  // Lifetoken
-    const [LifeToken] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", LifeToken.address);
-  
-    const lifeToken = await ethers.deployContract("LifeToken");
-  
-    console.log("Life Token address:", await lifeToken.getAddress());
 
   // SingleSwapToken
 
@@ -29,16 +12,7 @@ async function main(){
   
     console.log("SingleSwap address:", await singleSwapToken.getAddress());
 
-  // SwapMultiHop
-    const [SwapMultiHop] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", SwapMultiHop.address);
-  
-    const swapMultiHop = await ethers.deployContract("SwapMultiHop");
-  
-    console.log("MultiHopSwap address:", await swapMultiHop.getAddress());
-
-};
+}
 
 main()
   .then(() => process.exit(0))
